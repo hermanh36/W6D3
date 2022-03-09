a545f3dc1ecf5abe79eb58d67d476dce5a356842
Herman <hjiper2@DESKTOP-EU3DC6S.localdomain>
2022-03-09 16:36:55 -0500

    errors and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,5 +16,18 @@ class UsersController < ApplicationController
         user = User.find(params[:id])
         render json:user
     end
-    
+
+    def user_param
+        params.require(:users).permit(:name,:email)
+    end
+
+    def update
+        user = User.find(params[:id])
+        if user.update(user_param)
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
 end
