375db1b8f2b9b4d53f75748fcf8d57ab3265025c
Herman <hjiper2@DESKTOP-EU3DC6S.localdomain>
2022-03-09 15:53:33 -0500

    playing with parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,13 @@
-class UserController < ApplicationController
-    
+class UsersController < ApplicationController
+    def index
+        render plain: "This is the"
+    end
+
+    def create
+        render json:params
+    end
+
+    def show
+        render json:params
+    end
 end
